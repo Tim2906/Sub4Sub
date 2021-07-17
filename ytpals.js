b000f80cc73bbc3ecdbc57fbbf611effe816802c
Tim <69757707+Tim2906@users.noreply.github.com>
2021-07-17 17:23:02 +0200

    updated

--- ytpals.js
+++ ytpals.js
@@ -1,3 +1,45 @@
+// Method 1
+
+(function() {
+    'use strict';
+
+    if (window.location.href.indexOf("members-area/activate") != -1){
+        var xpath = '/html/body/div[1]/section/div/div/div/div/div/div[2]/div[1]/h2/span/div';
+        var checkForRemainingVideos = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerHTML;
+
+        if (checkForRemainingVideos > 0){
+            setTimeout(function(){check();}, 2000);
+            setTimeout(function(){location.reload();}, 3000);
+        }
+
+        if (checkForRemainingVideos < 1){
+            var confirmButton = document.evaluate('/html/body/div[1]/section/div/div/div/div/div/div[2]/div[2]/div[2]/div/div[2]/div[3]/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            var ytButton = document.evaluate('/html/body/div[1]/section/div/div/div/div/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+            ytButton.click();
+            startSub();
+
+            setTimeout(function(){confirmButton.click();}, 17000);
+
+        }
+    }
+
+    if (window.location.href.indexOf("free-activation-success") != -1){
+
+        var activateButton = doucment.evaluate('/html/body/div[1]/section/div/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+        location.href = "https://www.ytpals.com/members-area/";
+
+        setTimeout(function(){activateButton.click();}, 4,32e+7)
+
+        setTimeout(function(){location.href="https://www.ytpals.com/members-area/activate"}, 4.3236e+9);
+
+    }
+
+})();
+
+
+// Method 2
 (function() {
     'use strict';
 
